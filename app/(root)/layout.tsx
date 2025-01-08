8bcf7708f9114b21cf57e0c0854b9f7dda6171d2
ManojPrabhakar M <manojprabhakarm@ManojPrabhakars-MacBook-Pro.local>
2025-01-08 12:40:21 +0530

    onboarding page with backend functionality added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Topbar from "@/components/shared/Topbar";
